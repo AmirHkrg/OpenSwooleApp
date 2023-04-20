f97e5f08439ff88c973b3a41a4fa57a724b36b5f
onlineSchool <amirh.kargar85@gmail.com>
2023-04-20 18:53:24 +0330

    add cli help

--- core/client-use.php
+++ core/client-use.php
@@ -4,13 +4,17 @@ class Cli
 {
     protected function get_options(): void
     {
-        $options = getopt("S:");
+        $options = getopt("S:h::", ["help"]);
         foreach ($options as $key => $value) {
             $options[$key] = strtolower($value);
         }
 
-        if ($options["S"]) {
+        if (isset($options["S"])) {
             $this->validate_url($options);
+        }
+
+        if (isset($options["h"]) || isset($options["help"])){
+            $this->help();
         }
     }
 
@@ -45,6 +49,8 @@ class Cli
         $port = $data['port'];
         $fileName = $data['filename'];
 
+//        var_dump($data);
+
         if (pathinfo($fileName, PATHINFO_EXTENSION) !== 'php') {
             echo "The selected file must be php";
             exit();
@@ -61,5 +67,16 @@ class Cli
         $server_config['cli']['port'] = $port;
         $server_config['cli']['filename'] = $fileName;
         require_once $fileName;
+    }
+
+    protected function help(){
+        $help =
+            "Usage: php cli [-Option] [...Parameters]" . PHP_EOL .
+            "Options are:" . PHP_EOL .
+            "    -S         Address:Port--Filename  Start Server" . PHP_EOL .
+            "    -h --help                          Help" . PHP_EOL .
+            PHP_EOL
+        ;
+        echo $help;
     }
 }
